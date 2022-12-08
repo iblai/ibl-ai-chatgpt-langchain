3aab892b53d045feccb8fc342f0086fa288b2a49
Wang Zhao <wz@wangzhao.rocks>
2022-12-08 21:53:34 +0800

    modified for ibl-ai-pro

--- src/base.py
+++ src/base.py
@@ -4,26 +4,33 @@ import sys
 from dotenv import load_dotenv
 from langchain.llms.base import LLM
 from pyChatGPT import Chat
-from exceptions import IBLChatGPTError
 from pyChatGPT.classes.exceptions import Auth0Exception
+
+from exceptions import IBLChatGPTError
 
 load_dotenv()
 
 
-class CustomLLM(LLM):
+class IBLChatGPT(LLM):
     def __call__(self, prompt: str) -> str:
         try:
-            chat = Chat(email=os.environ["EMAIL"], password=os.environ["PASSWORD"])
+            chat = Chat(
+                email=os.environ["OPENAI_EMAIL"], password=os.environ["OPENAI_PASSWORD"]
+            )
         except Auth0Exception as e:
-            if("Password was incorrect" in str(e)):
-                raise IBLChatGPTError("It is likely you provided incorrect credentials. Check your email address and/or password.")
-            elif("an access token" in str(e)):
-                raise IBLChatGPTError("This is likely an issue with ChatGPT. Please try again later.")
+            if "Password was incorrect" in str(e):
+                raise IBLChatGPTError(
+                    "It is likely you provided incorrect credentials. Check your email address and/or password."
+                )
+            elif "an access token" in str(e):
+                raise IBLChatGPTError(
+                    "Too many people are using ChatGPT right now, so please try again later! :3"
+                )
         answer = chat.ask(prompt)
         return answer
 
 
-llm = CustomLLM()
-
-response  = llm(sys.argv[1])
-print(response)
+if __name__ == "__main__":
+    llm = IBLChatGPT()
+    response = llm(sys.argv[1])
+    print(response)
--- src/exceptions.py
+++ src/exceptions.py
@@ -1,5 +1,6 @@
 from pyChatGPT.classes.exceptions import Auth0Exception
 
+
 class IBLChatGPTError(Auth0Exception):
     def __str__(self):
         return self.message
